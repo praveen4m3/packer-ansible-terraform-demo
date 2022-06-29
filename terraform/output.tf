870c93e24bd08deb10901b6eee0d8ed3a5b2c54d
praveen4m3 <102419389+praveen4m3@users.noreply.github.com>
2022-06-29 16:32:17 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -6,6 +6,4 @@ output "curl" {
     value = "curl http://${aws_instance.ec2.public_ip}"
 }
 
-output "Login-with-Key" {
-    value = "ssh -i ${aws_key_pair.sshkey.key_name} ubuntu@${aws_instance.ec2.public_ip}"
-}
+
