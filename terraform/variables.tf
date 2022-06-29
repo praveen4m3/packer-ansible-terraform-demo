5f400a0d0e54322f009ae4bdd16c4503e320e360
praveen4m3 <102419389+praveen4m3@users.noreply.github.com>
2022-06-29 16:23:13 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,7 +16,7 @@ variable "vpc_cidr" {
 
 variable "subnet_cidr" {
     description = "Subnet CIDR blocks"
-    type = "list"
+    type = list(string)
     default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
