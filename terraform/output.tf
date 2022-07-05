f662a77b078169857b75f2a337e110b6156ea802
praveen4m3 <102419389+praveen4m3@users.noreply.github.com>
2022-07-05 14:48:04 +0530

    Create output.tf

---
+++ terraform/output.tf
@@ -0,0 +1,11 @@
+output "publicIp" {
+    value = "${aws_instance.ec2.public_ip}"
+}
+
+output "curl" {
+    value = "curl http://${aws_instance.ec2.public_ip}"
+}
+
+output "example-ami-packer" {
+  value = "${data.aws_ami.packer_image.id}"
+}
